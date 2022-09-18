d4c8bad98de4b6653a596d6cf96165494cbf3afd
Paul Hammant <paul@hammant.org>
2022-09-18 18:36:38 +0100

    toBe expectations

--- test/specs/app.js
+++ test/specs/app.js
@@ -1,16 +1,18 @@
 const assert = require('assert');
 
-describe('webdriver.io page', () => {
+describe('incr-decr component', () => {
     beforeEach(() => {
         browser.url('http://localhost:64308/iframe.html?id=app--with-text&viewMode=story');
     });
 
     it('should increase and decrease values by 1', () => {
-        expect($('.message-output').getText()).toBe('Current value: 0');
+        let message = $('.message-output');
+        expect(message.getText()).toBe('Current value: 0');
         $('#btnIncrement').click();
-        expect($('.message-output').getText()).toBe('Current value: 1');
-        $('#btnDecrement').click()
-        $('#btnDecrement').click()
-        expect($('.message-output').getText()).toBe('Current value: -1');
+        expect(message.getText()).toBe('Current value: 1');
+        let btnDecrement = $('#btnDecrement');
+        btnDecrement.click()
+        btnDecrement.click()
+        expect(message.getText()).toBe('Current value: -1');
     });
 });
